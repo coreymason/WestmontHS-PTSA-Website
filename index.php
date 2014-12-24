bcf0bae097edae84e8a093585e7495ff186dddf7
Corey Mason <corey.mason98@googlemail.com>
2014-12-23 22:10:32 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Westmont PTSA</title>
+  <meta name="description" content=""/>
+  <meta name="keywords" content="" />
+  <meta name="author" content="Corey Mason"/>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <meta property="og:title" content=""/>
+  <meta property="og:description" content=""/>
+  <meta property="og:type" content=""/>
+  <meta property="og:url" content=""/>
+  <meta property="og:image" content=""/>
+
+  <link href="style.css" rel="stylesheet">
+
+  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <header>
+
+  </header>
+
+  <footer>
+
+  </footer>
+
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js"></script>
+</body>
+</html>
---
+++ tidycms/server.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("opcache.enable", "0");
+ini_set("xcache.cacher", "0");
+ini_set("eaccelerator.enable", "0");
+ini_set("detect_unicode", "0");
+$version = "master.158-2c41869.2014-12-15";
+
+require_once('tidycms-connector.phar');
+
+?>
