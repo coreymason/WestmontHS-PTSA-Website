3c00df746bf943ce72ddfbb3ab170cd22ae8331e
Corey Mason <corey.mason98@googlemail.com>
2014-12-24 14:48:40 -0800

    Header

--- index.php
+++ index.php
@@ -24,7 +24,27 @@
 </head>
 <body>
   <header>
-
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Bulletin</a></li>
+              <li><a href="#">About Us</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
   </header>
 
   <footer>
